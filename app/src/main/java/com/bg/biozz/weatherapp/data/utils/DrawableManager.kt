e3ae1ef809516aa98c93314a44e657f2d221f412
Kirito398 <henkok398@mail.ru>
2019-05-06 11:23:35 +0300

    Vector animation 24

--- app/src/main/java/com/bg/biozz/weatherapp/data/utils/DrawableManager.kt
+++ app/src/main/java/com/bg/biozz/weatherapp/data/utils/DrawableManager.kt
@@ -24,7 +24,7 @@ class DrawableManager {
             "13n" -> R.drawable.ic_13n
             "50d" -> R.drawable.ic_50n
             "50n" -> R.drawable.ic_50n
-            else -> R.drawable.anim_ic_na
+            else -> R.drawable.ic_11d
             //R.drawable.ic_na
         }
     }
